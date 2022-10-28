986b775cb12ec5b4f97e3d5f31397d6b947426d8
Nuno do Carmo <nuno.carmo@suse.com>
2022-10-28 10:40:38 +0200

    Removed .docusaurus from Github files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\github\\elemental-docs\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\github\\elemental-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\github\\elemental-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\github\\elemental-docs\\src\\css\\custom.css'),
-];
