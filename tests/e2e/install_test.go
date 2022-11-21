c4978ffbc13abb9ef95b4d53900c3f3ff5e8d372
Itxaka <igarcia@suse.com>
2022-11-21 11:11:19 +0100

    afdimsei
    Signed-off-by: Itxaka <igarcia@suse.com>

--- tests/e2e/install_test.go
+++ tests/e2e/install_test.go
@@ -195,8 +195,10 @@ var _ = Describe("E2E - Install Rancher Manager", Label("install"), func() {
 		})
 
 		By("Patching fleet controller to enable debug", func() {
-			err := k.WaitForNamespaceWithPod("cattle-fleet-system", "app=fleet-controller")
-			Expect(err).To(Not(HaveOccurred()))
+			run, _ := kubectl.Run("get", "deployments", "--all-namespaces")
+			GinkgoWriter.Printf(run)
+			err := k.Wait("cattle-fleet-system", "Available=True", "deployment/fleet-controller", 120*time.Second)
+			Expect(err).ToNot(HaveOccurred())
 			r, err := kubectl.Run("patch", "deployment", "-n", "cattle-fleet-system", "fleet-controller", "--patch-file", fleetDebugYaml)
 			Expect(err).ToNot(HaveOccurred())
 			GinkgoWriter.Printf(r)
