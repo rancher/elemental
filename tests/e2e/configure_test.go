063a125f4d198725d19043f8eaf9969cfa9e4b51
Loic Devulder <ldevulder@suse.com>
2022-12-15 19:16:13 +0100

    ci/e2e: disable cluster check for worker pool
    In worker pool we can't check/run kubectl command directly on the node.
    
    Signed-off-by: Loic Devulder <ldevulder@suse.com>

--- tests/e2e/configure_test.go
+++ tests/e2e/configure_test.go
@@ -16,6 +16,7 @@ package e2e_test
 
 import (
 	"os/exec"
+	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -156,6 +157,17 @@ var _ = Describe("E2E - Configure test", Label("configure"), func() {
 				}, misc.SetTimeout(3*time.Minute), 5*time.Second).Should(ContainSubstring("machine-registration-" + pool + "-" + clusterName))
 			}
 		})
+
+		// Only for K3s cluster
+		if strings.Contains(k8sVersion, "k3s") {
+			By("Setting controlPlane role for worker pool", func() {
+				// Wait a little before toggling the role
+				time.Sleep(misc.SetTimeout(1 * time.Minute))
+
+				err := misc.ToggleRole(clusterNS, clusterName, "pool-worker-"+clusterName, "ControlPlaneRole", true)
+				Expect(err).To(Not(HaveOccurred()))
+			})
+		}
 
 		By("Starting HTTP server for network installation", func() {
 			// TODO: improve it to run in background!
