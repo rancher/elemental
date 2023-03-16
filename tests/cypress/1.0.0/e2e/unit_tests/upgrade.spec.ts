cfb294e2e62761223ea982124bd6f0230f8e91b8
Julien Adamek <julien.adamek@suse.com>
2023-03-16 14:39:42 +0100

    ci/ui: simplify upgrade test with elemental 1.0.0

--- tests/cypress/1.0.0/e2e/unit_tests/upgrade.spec.ts
+++ tests/cypress/1.0.0/e2e/unit_tests/upgrade.spec.ts
@@ -44,55 +44,6 @@ describe('Upgrade tests', () => {
     elemental.accessElementalMenu(); 
   });
   filterTests(['upgrade'], () => {
-    it('Create an OS Version Channels', () => {
-      // TODO: REMOVE 'IF' BLOCK AFTER NEXT STABLE VERSION (> 1.0.0)
-      // OSversion channel was not integrated in the UI before 1.1.0
-      if ( elementalUIVersion == '1.0.0' ) {
-        cy.exec(`sed -i 's/# namespace: fleet-default/namespace: fleet-default/g' assets/managedOSVersionChannel.yaml`);
-        cy.exec(`sed -i 's@image: %UPGRADE_CHANNEL_LIST%@image: ${upgradeChannelList}@g' assets/managedOSVersionChannel.yaml`);
-        cy.get('.nav')
-          .contains('Advanced')
-          .click();
-        cy.get('.nav')
-          .contains('OS Version Channels')
-          .click();
-        cy.clickButton('Create from YAML');
-        // Wait needed to avoid crash with the upload
-        cy.wait(2000);
-        cy.get('input[type="file"]')
-          .attachFile({filePath: '../../assets/managedOSVersionChannel.yaml'});
-        // Wait needed to avoid crash with the upload
-        cy.wait(2000);
-        // Wait for os-versions to be printed, that means the upload is done
-        cy.clickButton('Create');
-        // The new resource must be active
-        cy.contains('Active');
-      } else {
-        cy.get('.nav')
-          .contains('Advanced')
-          .click();
-        cy.get('.nav')
-          .contains('OS Version Channels')
-          .click();
-        cy.clickButton('Create');
-        cy.typeValue({label: 'Name', value: channelName});
-        cy.typeValue({label: 'Image registry path', value: upgradeChannelList});
-        cy.clickButton('Create');
-      }
-    });
-
-    it('Check OS Versions', () => {
-      cy.get('.nav')
-        .contains('Advanced')
-        .click();
-      cy.get('.nav')
-        .contains('OS Versions')
-        .click();
-      cy.contains('Active dev', {timeout: 120000});
-      cy.contains('Active stable');
-      cy.contains('Active staging');
-    });
-
     it('Upgrade one node (different methods if rke2 or k3s)', () => {
       // K3s cluster upgraded with OS Image
       // RKE2 cluster upgraded with OS version channel
@@ -110,25 +61,7 @@ describe('Upgrade tests', () => {
         .click();
       cy.contains(clusterName)
         .click();
-      // TODO: REMOVE FIRST 'IF' BLOCK AFTER NEXT STABLE VERSION (> 1.0.0)
-      // Following 'if' code will be removed once we get a new stable version
-      if (elementalUIVersion == '1.0.0') {
-        cy.typeValue({label: 'OS Image', value: upgradeImage});
-      } else {
-          if (checkK3s.test(k8sVersion)) {
-            cy.contains('Use image from registry')
-              .click();
-            cy.typeValue({label: 'Image path', value: upgradeImage});
-          } else {
-            cy.contains('Use Managed OS version')
-              .click();
-            cy.get(':nth-child(4) > .labeled-select')
-              .contains('Managed OS version')
-              .click();
-            cy.contains('staging')
-              .click();
-          };
-      }; 
+      cy.typeValue({label: 'OS Image', value: upgradeImage});
       cy.clickButton('Create');
       // Status changes a lot right after the creation so let's wait 10 secondes
       // before checking
@@ -169,39 +102,6 @@ describe('Upgrade tests', () => {
         .contains('Updating', {timeout: 240000});
       cy.get('.primaryheader')
         .contains('Active', {timeout: 240000});
-    });
-
-    it('Delete OS Versions', () => {
-      cy.get('.nav').contains('Advanced')
-        .click();
-      cy.get('.nav')
-        .contains('OS Versions')
-        .click();
-      cy.contains('dev')
-        .parent()
-        .parent()
-        .click();
-      cy.clickButton('Delete');
-      cy.confirmDelete();
-      cy.contains('dev')
-        .should('not.exist');
-    });
-
-    it('Delete OS Versions Channels', () => {
-      cy.get('.nav')
-        .contains('Advanced')
-        .click();
-      cy.get('.nav')
-        .contains('OS Version Channels')
-        .click();
-      cy.deleteAllResources();
-      cy.get('.nav')
-        .contains('Advanced')
-        .click();
-      cy.get('.nav')
-        .contains('OS Versions')
-        .click();
-      cy.contains('There are no rows to show');
     });
   });
 });
