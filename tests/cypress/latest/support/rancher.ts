e03fdc776e4793d92f109d77bf0f6ad92d9e7766
Julien Adamek <julien.adamek@suse.com>
2023-07-10 12:05:53 +0200

    ci/ui: Add documentation according to tsdoc syntax

--- tests/cypress/latest/support/rancher.ts
+++ tests/cypress/latest/support/rancher.ts
@@ -16,18 +16,52 @@ limitations under the License.
 // /////////////////
 
 export class Rancher {
+  /**
+   * Click on the menu given as parameter
+   * @remarks : Mostly use in the burger menu
+   * @param menu : The menu to click on 
+   */
   accesMenu(menu: string) {
     cy.contains(menu)
       .click();
   }
 
+  /**
+   * Add a Helm repository
+   * @remarks : The repository is added to the local cluster
+   * @param repositoryName : Name of the repository
+   * @param repositoryURL : URL of the repository
+   * @param repositoryType : Type of the repository
+   */
   addRepository(repositoryName: string, repositoryURL: string, repositoryType: string) {
     this.burgerMenuOpenIfClosed();
     cy.contains('local')
       .click();
-    cy.addHelmRepo(repositoryName, repositoryURL, repositoryType);
+      cy.clickNavMenu(['Apps', 'Repositories'])
+    // Make sure we are in the 'Repositories' screen (test failed here before)
+    cy.contains('header', 'Repositories')
+      .should('be.visible');
+    cy.contains('Create')
+      .should('be.visible');
+
+    cy.clickButton('Create');
+    cy.contains('Repository: Create')
+      .should('be.visible');
+    cy.typeValue('Name', repositoryName);
+    if (repositoryType === 'git') {
+      cy.contains('Git repository')
+        .click();
+      cy.typeValue('Git Repo URL', repositoryURL);
+      cy.typeValue('Git Branch', 'main');
+    } else {
+      cy.typeValue('Index URL', repositoryURL);
+    }
+    cy.clickButton('Create');
   };
 
+  /**
+   * Check if the burger menu is open and open it if it's closed
+   */
   burgerMenuOpenIfClosed() {
     cy.get('body').then((body) => {
       if (body.find('.menu.raised').length === 0) {
@@ -36,11 +70,22 @@ export class Rancher {
     });
   };
 
+  /**
+   * Click on the burger menu on the top left of the screen
+   * @remarks : Used in burgerMenuOpenIfClosed()
+   */
   burgerMenuToggle() {
     cy.getBySel('top-level-menu', {timeout: 12000})
       .click();
   };
 
+  /**
+   * Check if the cluster is in the expected state
+   * @remarks : Checked in the Home page
+   * @param clusterName : Name of the cluster
+   * @param clusterStatus : Expected status of the cluster
+   * @param timeout : Timeout for the check
+   */
   checkClusterStatus(clusterName: string, clusterStatus: string, timeout: number) {
     this.burgerMenuOpenIfClosed();
       cy.contains('Home')
@@ -50,10 +95,32 @@ export class Rancher {
         .contains(clusterStatus,  {timeout: timeout});
   };
 
+  /**
+   * Check if the expected icon appears
+   * @remarks : Mainly used in the burger menu
+   * @param iconName : Name of the icon
+   * @returns : The icon
+   */
   checkNavIcon(iconName: string) {
     return cy.get('.option .icon.group-icon.icon-'+iconName);
   } 
   
+  /**
+   * Confirm the deletion of an object
+   */
+  confirmDelete() {
+    cy.getBySel('prompt-remove-confirm-button')
+    .click();
+  }
+
+  /**
+   * Create an user for the Rancher UI
+   * @remarks : Only one role can be given for now
+   * @param username : Name of the user
+   * @param password : Password of the user
+   * @param role : Role of the user
+   */
+  // TODO: Add the possibility to add multiple roles
   createUser(username: string, password: string, role?: string) {
     this.burgerMenuOpenIfClosed();
     cy.contains('Users & Authentication')
@@ -73,6 +140,11 @@ export class Rancher {
       .click();
   }
 
+  /**
+   * Enable the extension support
+   * @remarks : Disable the Rancher Repo if you provide your own repo
+   * @param withRancherRepo : Add the Rancher Extension Repository
+   */
   enableExtensionSupport(withRancherRepo: boolean) {
     cy.contains('Extensions')
       .click();
@@ -89,7 +161,9 @@ export class Rancher {
     .contains('Installed Available Updates All');
   };
 
-  // Handle first login in Rancher
+  /**
+   * Do the first login to Rancher UI, accept the terms and conditions etc
+   */
   firstLogin() {
     cy.visit('/auth/login');
     cy.get("span").then($text => {
@@ -115,19 +189,64 @@ export class Rancher {
 declare global {
   namespace Cypress {
     interface Chainable {
-      addHelmRepo(repoName: string, repoUrl: string, repoType?: string,): Chainable<Element>;
+      /**
+       * Search for an element by its label
+       * @param label : Label of the element
+       */
       byLabel(label: string,): Chainable<Element>;
+
+      /**
+       * Click on a button by its label
+       * @param label : Label of the button
+       */
       clickButton(label: string,): Chainable<Element>;
+
+      /**
+       * Go into the given menu
+       * @remarks : You can access submenu by giving submenu name in the array (ex: cy.clickNavMenu(['Menu', 'Submenu']))
+       * @param listLabel : List of the menu names
+       */
       clickNavMenu(listLabel: string[],): Chainable<Element>;
+
+      /**
+       * Confirm the deletion of a resource
+       */
       confirmDelete(): Chainable<Element>;
+
+      /**
+       * Delete all resources from the current page
+       */
       deleteAllResources():Chainable<Element>;
+
+      /**
+       * Login to Rancher UI
+       * @remarks : Not sure cacheSession is still used/needed
+       * @param username : Username of the user
+       * @param password : Password of the user
+       * @param cacheSession : Cache the session
+       */
       login(username?: string, password?: string, cacheSession?: boolean,): Chainable<Element>;
+
+      /**
+       * Yields elements with a data-test attribute that match a specified selector.
+       * @remarks : data-testid attribute is used in the Rancher codebase to identify elements 
+       * @param dataTestAttribute : Attribute of the element 
+       * @param args : Other arguments to pass
+       * @returns : The element
+       */
       getBySel(dataTestAttribute: string, args?: any): Chainable<JQuery<HTMLElement>>;
+
+      /**
+       * Insert a value in a field
+       * @param label : Label of the field
+       * @param value : Value to insert
+       * @param noLabel : If true, the label is not used
+       * @param log : If true, the value is logged
+       */
       typeValue(label: string, value: string, noLabel?: boolean, log?: boolean): Chainable<Element>;
     }
 }}
 
-// Log into Rancher
 Cypress.Commands.add('login', (
   username = Cypress.env('username'),
   password = Cypress.env('password'),
@@ -157,39 +276,31 @@ Cypress.Commands.add('login', (
     }
 });
 
-// Make sure we are in the desired menu inside a cluster (local by default)
-// You can access submenu by giving submenu name in the array
-// ex:  cy.clickNavMenu(['Menu', 'Submenu'])
 Cypress.Commands.add('clickNavMenu', (listLabel: string[]) => {
   listLabel.forEach(label => cy.get('nav').contains(label).click());
 });
 
-// Search by data-testid selector
 Cypress.Commands.add("getBySel", (selector, ...args) => {
   return cy.get(`[data-testid=${selector}]`, ...args);
 });
 
-// Search fields by label
 Cypress.Commands.add('byLabel', (label) => {
   cy.get('.labeled-input')
     .contains(label)
     .siblings('input');
 });
 
-// Search button by label
 Cypress.Commands.add('clickButton', (label) => {
   cy.get('.btn')
     .contains(label)
     .click();
 });
 
-// Confirm the delete operation
 Cypress.Commands.add('confirmDelete', () => {
   cy.getBySel('prompt-remove-confirm-button')
     .click()
 });
 
-// Insert a value in a field *BUT* force a clear before!
 Cypress.Commands.add('typeValue', (label, value, noLabel, log=true) => {
   if (noLabel === true) {
     cy.get(label)
@@ -204,7 +315,6 @@ Cypress.Commands.add('typeValue', (label, value, noLabel, log=true) => {
   }
 });
 
-// Delete all resources from a page
 Cypress.Commands.add('deleteAllResources', () => {  
   cy.get('[width="30"] > .checkbox-outer-container')
     .click();
@@ -222,30 +332,4 @@ Cypress.Commands.add('deleteAllResources', () => {
       };
     });
   cy.contains('There are no rows to show', {timeout: 15000});
-});
-
-// Add Helm repo
-Cypress.Commands.add('addHelmRepo', (repoName, repoUrl, repoType) => {
-  //cy.clickClusterMenu(['Apps', 'Repositories'])
-  cy.clickNavMenu(['Apps', 'Repositories'])
-
-  // Make sure we are in the 'Repositories' screen (test failed here before)
-  cy.contains('header', 'Repositories')
-    .should('be.visible');
-  cy.contains('Create')
-    .should('be.visible');
-
-  cy.clickButton('Create');
-  cy.contains('Repository: Create')
-    .should('be.visible');
-  cy.typeValue('Name', repoName);
-  if (repoType === 'git') {
-    cy.contains('Git repository')
-      .click();
-    cy.typeValue('Git Repo URL', repoUrl);
-    cy.typeValue('Git Branch', 'main');
-  } else {
-    cy.typeValue('Index URL', repoUrl);
-  }
-  cy.clickButton('Create');
 });
