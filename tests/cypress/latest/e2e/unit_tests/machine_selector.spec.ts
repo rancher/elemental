4904f912b809d98999d313f470bd9098762cfd38
Julien Adamek <julien.adamek@suse.com>
2023-09-08 10:11:25 +0200

    ci/ui: add missing blank line after copyright

--- tests/cypress/latest/e2e/unit_tests/machine_selector.spec.ts
+++ tests/cypress/latest/e2e/unit_tests/machine_selector.spec.ts
@@ -11,6 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 import { Elemental } from '~/support/elemental';
 import '~/support/commands';
 import filterTests from '~/support/filterTests.js';
