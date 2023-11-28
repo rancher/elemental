554f7260ca868cbf1217c6c53ad08bd15753ed0c
Loic Devulder <ldevulder@suse.com>
2023-11-28 12:10:46 +0100

    ci/cli: fix tests with Rancher Manager 2.8-HEAD
    Elemental operator should be installed with Helm on all CLI tests.
    
    Signed-off-by: Loic Devulder <ldevulder@suse.com>

--- tests/e2e/install_test.go
+++ tests/e2e/install_test.go
@@ -317,7 +317,7 @@ var _ = Describe("E2E - Install Rancher Manager", Label("install"), func() {
 		// Deploy operator in CLI test or if Rancher version is < 2.8
 		// because operator can not be installed trough Marketplace in Rancher 2.7.x
 		matched, _ := regexp.MatchString(`2.8`, rancherHeadVersion)
-		if testType == "cli" || matched == false {
+		if strings.Contains(testType, "cli") || matched == false {
 			By("Installing Elemental Operator", func() {
 				for _, chart := range []string{"elemental-operator-crds", "elemental-operator"} {
 					RunHelmCmdWithRetry("upgrade", "--install", chart,
