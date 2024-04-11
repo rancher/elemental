03738695f7fb5a095eab2ffb15fa9f56448621c8
Julien Adamek <julien.adamek@suse.com>
2024-04-11 11:22:14 +0200

    ci/ui: add ui-plugin for rancher 2.8 and 2.9

--- tests/cypress/latest/e2e/unit_tests/elemental_plugin.spec.ts
+++ tests/cypress/latest/e2e/unit_tests/elemental_plugin.spec.ts
@@ -36,7 +36,7 @@ filterTests(['main', 'upgrade'], () => {
 
     // Add rancher-ui-plugin-charts repo because its part of Rancher Prime in 2.8 and 2.9-head
     it('Add rancher-ui-plugin-charts repo', () => {
-      isRancherManagerVersion('2.8-head') || isRancherManagerVersion('2.9-head') ? cypressLib.addRepository('rancher-ui-plugin-charts', 'https://github.com/rancher/ui-plugin-charts.git', 'git', 'main') : null;
+      isRancherManagerVersion('2.8') || isRancherManagerVersion('2.9') ? cypressLib.addRepository('rancher-ui-plugin-charts', 'https://github.com/rancher/ui-plugin-charts.git', 'git', 'main') : null;
     });
     
     qase(12,
