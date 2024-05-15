baffc6b0d682fdab3c009908532b79743189635b
Julien Adamek <julien.adamek@suse.com>
2024-05-15 14:03:28 +0200

    ci/ui: bump Cypress container to 13.9.0

--- tests/cypress/latest/cypress.config.ts
+++ tests/cypress/latest/cypress.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     'projectCode': 'ELEMENTAL',
     'logging': true,
   },
+  video: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
